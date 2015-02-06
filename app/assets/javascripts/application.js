bb8a3e629ba1331dfab0485a6f75d2cb9253ebd8
Avramov Vsevolod <Avramov-VA@mail.vsb.sbrf.ru>
2015-02-06 16:10:48 +0800

    complete date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,13 @@
 //= require moment
 //= require underscore
 //= require angular
+//= require angular-i18n/angular-locale_ru-ru.js
 //= require angular-bootstrap
 //= require angular-moment
 //= require angular-ui-router
 //= require jsmodules/abn_tree_directive.js
 //= require angular-breadcrumb
 //= require angular-rails-templates
-//= require angular-bootstrap-datetimepicker/datetimepicker.js
 //= require moment/locale/ru.js
 //= require_tree ./templates
 //= require angularjs/rails/resource
@@ -20,11 +20,10 @@
 moment.locale('ru');
 var URSKBApp = angular.module('URSKBApp', [
       'angularBootstrapNavTree',
-      'ui.bootstrap.datetimepicker',
+      'ui.bootstrap',
       'dictionary.services',
       'indicator.services',
       'client.services',
-      'ui.bootstrap',
       //'new.edit',
       'templates',
       'ui.router',
--- app/assets/javascripts/controllers/client_indicator_filter_ctrl.coffee
+++ app/assets/javascripts/controllers/client_indicator_filter_ctrl.coffee
@@ -1,7 +1,7 @@
 URSKBApp
   .controller 'ClientIndicatorFilterCtrl', ['$state', '$rootScope', '$stateParams', '$scope', 'Client', 'Indicator', ($state, $rootScope, $stateParams, $scope, Client, Indicator) ->
     $scope.indicatorsTree = []
-    $scope.date = new Date
+    $scope.date = new Date()
 
     Indicator.tree($scope.date).then (result) ->
       $scope.indicatorsTree = result
@@ -13,4 +13,13 @@ URSKBApp
       if oldDate != newDate
         $rootScope.filterDate = newDate
 
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    }
+
+    $scope.open = ($event) ->
+      $event.preventDefault()
+      $event.stopPropagation()
+      $scope.opened = true
   ]
